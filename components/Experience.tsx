5f8c18edaaefd7ddb1835a2e9fc0c72036f458a6
Sarthak Kumar Behera <sarthakbehera09@gmail.com>
2024-12-12 21:36:30 +0530

    esl dsb 2

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { workExperience } from "@/data";
 import React from "react";
 import { Button } from "./ui/MovingBorder";
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
 import MagicButton from "./ui/MagicButton";
 import { FaLocationArrow } from "react-icons/fa6";
@@ -39,7 +40,12 @@ const Footer = () => {
               rel="noopener noreferrer"
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
-              <img src={profile.img} alt={profile.id} width={20} height={20} />
+              <img
+                src={profile.img}
+                alt={`${profile.id} `}
+                width={20}
+                height={20}
+              />
             </Link>
           ))}
         </div>
